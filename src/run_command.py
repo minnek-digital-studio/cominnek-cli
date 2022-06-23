cb75e9f2e8e27c0576c0fa7932578cf6ebe20718
Isaac Martinez <isaac@minnekdigital.com>
2022-06-23 08:28:19 -0400

    feat: support to unix

--- src/run_command.py
+++ src/run_command.py
@@ -1,7 +1,21 @@
 import subprocess
+from . import config
 
 def run_command(command, output=False):
+    if(config.is_windows):
+        return _run_command_windows(command, output)
+    else:
+        return _run_command_unix(command, output)
+
+
+def _run_command_windows(command, output=False):
     if(output):
         return str(subprocess.check_output(['powershell.exe', command]))
     else:
         subprocess.call(['powershell.exe', command])
+
+def _run_command_unix(command, output=False):
+    if(output):
+        return str(subprocess.check_output(command, shell=True))
+    else:
+        subprocess.call(command, shell=True)
