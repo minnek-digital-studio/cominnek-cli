f12cf8d643ef2c4a1e4ce3c1ef3211f50ebf7995
Isaac Martinez <isaac@minnekdigital.com>
2022-07-07 12:43:21 -0400

    fix: error handeling
    now it'detecect if the branch have any conflict to prvent continue with errors

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cominnek",
-    version="1.2.0",
+    version="1.2.1",
     packages=find_packages(),
     url="",
     license="MIT License",
