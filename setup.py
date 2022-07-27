beab28ee0e0a1a9e2bbe5972cf665f21da1522db
Isaac Martinez <isaac@minnekdigital.com>
2022-07-27 17:41:52 -0400

    fix update-version commit message

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cominnek",
-    version="1.3.1",
+    version="1.3.2",
     packages=find_packages(),
     url="https://github.com/Minnek-Digital-Studio/cominnek",
     license="MIT License",
