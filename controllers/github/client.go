198ad6508e10b2c22e00020656a61ce60b454fd7
Isaac Martinez <isaac@minnekdigital.com>
2022-10-16 16:11:15 -0400

    feat: update client error desing

--- controllers/github/client.go
+++ controllers/github/client.go
@@ -6,7 +6,9 @@ import (
 	"os"
 
 	"github.com/Minnek-Digital-Studio/cominnek/controllers/files"
+	"github.com/Minnek-Digital-Studio/cominnek/controllers/loading"
 	"github.com/Minnek-Digital-Studio/cominnek/controllers/security"
+	"github.com/fatih/color"
 	"github.com/google/go-github/v47/github"
 	"golang.org/x/oauth2"
 )
@@ -14,8 +16,9 @@ import (
 var ctx context.Context = context.Background();
 
 func client()* github.Client {
-	if (!files.CheckExist(FileName)) {
-		fmt.Println("No token found, please login. run 'cominnek auth login'")
+	if (!files.CheckExist(FileName) ) { 
+		loading.Stop()
+		fmt.Println("No token found, please login. run", color.HiGreenString("cominnek auth login"))
 		os.Exit(1)
 		return nil
 	}
