86ac75e8917ccbbe800b8905d0327d71b67957c4
Isaac Martinez <isaac@minnekdigital.com>
2023-03-11 21:05:57 -0400

    fix: show help if user leave empty option

--- cmd/update_version.go
+++ cmd/update_version.go
@@ -9,6 +9,12 @@ var updateVersion = &cobra.Command{
 	Use:   "update-version <version>",
 	Short: "Create a commit for update version following conventional commits",
 	Run: func(cmd *cobra.Command, args []string) {
+
+		if len(args) == 0 {
+			cmd.Help()
+			return
+		}
+
 		version := args[0]
 		extras.UpdateVersion(version)
 	},
